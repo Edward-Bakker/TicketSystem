a3e3f6e6c80d4247d9d4ba7e04309d10a947a367
Matts-cloud <71218200+Matts-cloud@users.noreply.github.com>
2020-12-16 14:01:40 +0000

    made an admin overview page
    admin overview page complete and working on an edit page

---
+++ www/admin.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forex Ninja Admin Page</title>
+    <style>
+    table, th, td{
+    border: 1px solid black;
+    }
+    </style>
+</head>
+<body>
+<h1>Admin Page</h1>
+    <?php
+    $link = mysqli_connect("localhost", "root", "", "ticketsystem")
+    OR Die("Could not connect to database!" . mysqli_error($link));
+    
+    $sql = "SELECT * FROM accounts ORDER BY id ASC";
+        $stmt = mysqli_query($link, $sql);
+        echo "<br>";
+        echo "<table>";
+        echo "<tr><th>" . "ID" . "</th><th>" . "name" . "</th><th>" . "email" . "</th><th>" . "password hash" . "</th><th>" . "adminlevel" . "</th><th>" . "approved" . "</th><th>" . "last login" . "</th><th>" . "Edit" . "</th></tr>";
+        while ($row = mysqli_fetch_array($stmt)) 
+        {
+            echo "<tr><td>" . $row["id"] . "</td><td>" . $row["name"] . "</td><td>" . $row["email"] . "</td><td>" . $row["password"] . "</td><td>" . $row["adminlevel"] . "</td><td>" . $row["approved"] . "</td><td>" . $row["last_login"] . "</td><td>" . "<a href= 'php/admineditpage.php?id=" . $row["id"] . "'>EDIT</a></td></tr>";                                                         
+        }
+        echo "</table>";
+        mysqli_close($link);
+    
+    ?>
+</body>
+</html>
