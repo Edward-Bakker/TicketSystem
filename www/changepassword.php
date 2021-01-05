5fb2edf80bee72fef4b15d32105949e1c7e95607
Atanas <atanas910@gmail.com>
2021-01-05 13:29:50 +0100

    Trying to make the changepassword work

--- www/changepassword.php
+++ www/changepassword.php
@@ -1,4 +1,4 @@
-<?php require 'php/autoloader.php'; 
+<?php require 'php/autoloader.php';
 session_start();
 if(($_SESSION["valid"] == false))
 {
@@ -26,8 +26,11 @@ if($values["approved"] === "0")
 	mysqli_close($link);
 }else
 {
-    
+
 }
+
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -39,18 +42,64 @@ if($values["approved"] === "0")
 <body>
     <h1>Change Password</h1>
     <hr>
-    
+
     <form name="Input" method="POST">
             <input type="Text" name="oldpassword" value=""> <b>Old Password</b> <br>
             <br>
             <input type="Text" name="newpassword" value=""> <b>New Password</b> <br>
             <br>
             <input type="Text" name="reppassword" value=""> <b>Repeat Password</b> <br>
+            <input type="submit" name="submit"> <br>
     </form>
 
 <?php
+ if(isset($_POST['submit']))
+ {
 $oldPassword = $_POST["oldpassword"];
+$newPassWord= $_POST['newpassword'];
+$repeatPassWord= $_POST['reppassword'];
+$passwordmatch=false;
+if( $newPassWord==$repeatPassWord){     $passwordmatch=true;}
+else { echo "The new passwords dont match";}
+if($newPassWord==$oldPassword) {echo "The new password is the same as the old one ";}
+if($passwordmatch==true)
+{
+$config = config::getDBConfig();
+$link = mysqli_connect($config->db_host, $config->db_user, $config->db_pass, $config->db_name)
+OR Die("Could not connect to database!" . mysqli_error($link));
+$sql = "SELECT  id,password FROM accounts WHERE id = ?";
+
+if ($stmt = $link->prepare($sql)) {
+    $stmt->bind_param('s', $id);
+    $stmt->execute()
+            or die("Could not send the data to the database: " . $link->errno);
+$stmt->bind_result($id, $password);
+$stmt->store_result();
+echo $password;
+
+
+if(password_verify($oldPassword,$password))
+{
+   $sql= "UPDATE accounts SET password = ? Where id= ?";
+   if($stmt=$link->prepare($sql)){
+       $stmt->bind_param('ss',password_hash($newPassWord, PASSWORD_DEFAULT), $id );
+       $stmt->execute()
+          or die ("Could not update the password ". $link->errno);
+          $stmt= mysqli_query($link, $sql);
+        echo "Reached checkpoint";
+   }
+
+}
+else { echo "The password doesnt match the old one ";
+
+}
+}
+}
+ }
 ?>
+
+
+
 
 </body>
 </html>
