5d7364dca6bac8e827b06eb80f4102f91769cc26
vaskosvet <63131710+vaskosvet@users.noreply.github.com>
2021-01-06 18:59:12 +0200

    Submiting tickets to the database finished

--- www/submit.php
+++ www/submit.php
@@ -1,22 +1,26 @@
-<?php
+<?php require 'php/autoloader.php';
  
-$db_host = 'localhost';
-$db_user = 'ticketsystem_user';
-$db_pass = 'changeme';
-$db_name = 'ticketsystem';
+$db_host = "localhost";
+$db_user = "ticketsystem_user";
+$db_pass = "changeme";
+$db_name = "ticketsystem";
  
-$conn = mysqli_connect($db_host, $db_user, $db_pass,$db_name)
-OR Die("Could not connect to database!" . mysqli_error($conn));
+$conn = new mysqli($db_host, $db_user, $db_pass,$db_name) or die("Connect failed: %s\n". $conn -> error);
 
-$title = $_POST['title'];
-$question = $_POST['question'];
+session_start();
 
-
-$sql = "INSERT into tickets ('subject', 'content') values ($title, $question);";
-mysqli_query($conn,$sql);
+$title = mysqli_real_escape_string($conn, $_POST['title']);
+$question = mysqli_real_escape_string($conn, $_POST['question']);
+$id = mysqli_real_escape_string($conn, $_SESSION['id']);
 
 
-
-    
-
-?>
+$sql = "INSERT into tickets(`user_id`, `subject`, `content`) VALUES (?, ?, ?);";
+$stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)){
+        echo'Database error';
+    } else {
+        mysqli_stmt_bind_param($stmt, "sss", $id, $title, $question);
+        mysqli_stmt_execute($stmt);
+        header("Location:createnewticket.php?success");
+    }
+   //$title = filter_input(INPUT_POST, 'title', FILTER_DEFAULT);
